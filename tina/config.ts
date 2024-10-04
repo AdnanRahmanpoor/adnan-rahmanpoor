f59b56ba3a6be39cb0dd69e56a7ca95fb638e8ce
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-05 01:44:05 +0400

    Changed mediaRoot path and new tina-lock

--- tina/config.ts
+++ tina/config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "~/src/assets/images/",
+      mediaRoot: "src/assets/images/",
       publicFolder: "public",
       static: false,
     },
