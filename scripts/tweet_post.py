bdf4a6a632cdcb5c9539ebf4a952de6cbbca84e4
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-08 03:35:22 +0400

    more updates to tweet_post.py

--- scripts/tweet_post.py
+++ scripts/tweet_post.py
@@ -23,10 +23,11 @@ def get_latest_post():
                 post_title = line.strip('#').strip()
                 break
 
-            post_slug = re.sub(r'[^a-zA-Z0-9-]', post_title.lower().replace('','-'))
-            post_url = f'https://adnanrp.pages.dev/{post_slug}'
+                post_slug = re.sub(r'[^a-zA-Z0-9-]', post_title.lower().replace('','-'))
+                post_url = f'https://adnanrp.pages.dev/{post_slug}'
 
             return post_title, post_url
+    return None, None
 
 # post tweet
 
