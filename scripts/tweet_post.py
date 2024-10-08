ea679cfc983720ee9972ae4e4d19eb31631c96e4
Adnan Rahmanpoor <51033004+AdnanRahmanpoor@users.noreply.github.com>
2024-10-08 06:09:05 +0400

    Update tweet_post.py

--- scripts/tweet_post.py
+++ scripts/tweet_post.py
@@ -5,13 +5,14 @@ import re
 
 # auth
 auth = tweepy.OAuth1UserHandler(
+    os.getenv('bearer_token'),
     os.getenv('consumer_key'),
     os.getenv('consumer_secret'),
     os.getenv('access_token'),
     os.getenv('token_secret')
 )
 
-api = tweepy.API(auth)
+api = tweepy.Client(auth)
 
 def get_latest_post():
     # Get the latest post based on creation time
@@ -41,7 +42,7 @@ def get_latest_post():
 def tweet_new_post(post_title, post_url):
     if post_title and post_url:
         tweet = f"New Post: {post_title} - Read more at {post_url}"
-        api.update_status(tweet)
+        api.create_tweet(tweet)
     else:
         print("No post found to tweet.")
 
