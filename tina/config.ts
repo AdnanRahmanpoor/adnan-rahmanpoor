f1fa2d3b0f85f90394fda0153de18fa225760ba8
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-08 22:49:55 +0400

    changed TinaCMS mediaroot dir

--- tina/config.ts
+++ tina/config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "src/assets/images/",
+      mediaRoot: "_astro",
       publicFolder: "",
       static: false,
     },
