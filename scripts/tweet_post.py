c375b8ccbc91274fadca5962a9abf94caeab0b22
Adnan Rahmanpoor <51033004+AdnanRahmanpoor@users.noreply.github.com>
2024-10-08 06:02:06 +0400

    Update tweet_post.py

--- scripts/tweet_post.py
+++ scripts/tweet_post.py
@@ -41,7 +41,7 @@ def get_latest_post():
 def tweet_new_post(post_title, post_url):
     if post_title and post_url:
         tweet = f"New Post: {post_title} - Read more at {post_url}"
-        api.status(tweet)
+        api.update_status(tweet)
     else:
         print("No post found to tweet.")
 
