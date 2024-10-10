c58123ad6893c07e0b23d53b4d299faf128a4897
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-10 15:50:21 +0400

    updated tweet_post

--- scripts/tweet_post.py
+++ scripts/tweet_post.py
@@ -5,14 +5,14 @@ import re
 
 last_tweeted_file = 'last_tweeted.txt'
 
-# setting api to 2.0
+# Setting API to 2.0
 api = tweepy.Client(
     os.getenv('bearer_token'),
     os.getenv('consumer_key'),
     os.getenv('consumer_secret'),
     os.getenv('access_token'),
     os.getenv('token_secret')
-    )
+)
 
 def generate_slug(title):
     """
@@ -26,11 +26,10 @@ def generate_slug(title):
     slug = slug.replace(' ', '-')
     slug = re.sub(r'[^a-z0-9-]', '', slug)
     slug = re.sub(r'-+', '-', slug)
-
     return slug
 
 def get_all_posts():
-    # Get the latest post based on creation time
+    """Retrieve all posts and their titles, URLs, and slugs."""
     post_files = glob.glob("src/content/post/*.md")
     posts = []
 
@@ -38,28 +37,28 @@ def get_all_posts():
         with open(post_file, 'r') as f:
             post_title = None
             for line in f:
-                # Find the first line starting with '#' (post title)
+                # Find the title line in the markdown file
                 if line.startswith('title:'):
                     post_title = line.replace('title:', '').strip()
                     # Create slug from post title
                     post_slug = generate_slug(post_title)
                     post_url = f'https://adnanrp.pages.dev/{post_slug}'
+                    posts.append((post_title, post_url, post_slug))
                     break  # Exit the loop after finding the title
 
     return posts
 
 def get_tweeted_slugs():
-    """Read slug of the last tweeted post from the file"""
-
+    """Read slugs of the previously tweeted posts from the file."""
     if os.path.exists(last_tweeted_file):
         with open(last_tweeted_file, 'r') as f:
-            return set(f.read().splitlines())
+            return set(f.read().splitlines())  # Return a set of slugs for quick lookup
     return set()
 
-def set_last_tweeted(post_slug):
-    """Write the slug of the last tweeted post to the file"""
+def set_tweeted_slugs(tweeted_slugs):
+    """Write the slugs of all tweeted posts to the file."""
     with open(last_tweeted_file, 'w') as f:
-        f.write('\n'.join(post_slug))
+        f.write('\n'.join(tweeted_slugs))  # Join the slugs by newlines
 
 
 # Post tweet
@@ -75,14 +74,22 @@ def tweet_new_post(post_title, post_url):
         print("No post found to tweet.")
 
 
+# Get all posts
 posts = get_all_posts()
+
+# Get slugs of previously tweeted posts
 tweeted_slugs = get_tweeted_slugs()
+
+# Set to store newly tweeted slugs
 new_tweeted_slugs = set()
 
+# Iterate through posts and tweet any new ones
 for post_title, post_url, post_slug in posts:
     if post_slug not in tweeted_slugs:
+        # New post detected, tweet it
         tweet_new_post(post_title, post_url)
         new_tweeted_slugs.add(post_slug)
 
+# Update the slugs file with newly tweeted slugs
 tweeted_slugs.update(new_tweeted_slugs)
-set_last_tweeted(tweeted_slugs)
+set_tweeted_slugs(tweeted_slugs)
